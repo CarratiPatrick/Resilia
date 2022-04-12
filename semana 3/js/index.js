1321adae186fbf3d94a94535c2346a2b431b3cdf
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-04-12 16:30:26 -0300

    salvando alterações

--- semana 3/js/index.js
+++ semana 3/js/index.js
@@ -3,3 +3,15 @@ console.log ("Resilia ")
 console.log ("WebDev ")
 console.log ("2.0")
 console.log ("Resilia " + "WebDev " + "2.0")
+/*var alimento = prompt("qual o alimento")
+var precoSemanaAnterior = prompt("qual era o  preço?")
+var precoSemanaAtual = prompt("qual o preço atual? ")
+var diminuiu = precoSemanaAnterior - precoSemanaAtual
+
+if (  precoSemanaAtual > precoSemanaAnterior) {
+    console.log("o " + alimento + " aumentou");
+}else if ( precoSemanaAnterior - precoSemanaAtual ){
+    console.log("diminuiu " + diminuiu);
+}else{
+    console.log("aumentou");
+}*/
---
+++ semana 4/função/funcoes.js
@@ -0,0 +1,13 @@
+function dirigirEBeber(Idade) {
+ if(Idade >= 18 ){
+     console.log("Já pode dirigir e beber!");
+ } else{
+     console.log("Não pode dirigir");
+ }
+         
+}
+
+function previsaoRodagem (litros,km) {
+    return (litros/km)
+    
+}
