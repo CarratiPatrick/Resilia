eb4eaf77f4dd9129e3b33dbbd00b7733d9bea061
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-04-21 13:12:27 -0300

    ajustes do css

--- semana 4/função/funcoes.js
+++ semana 4/função/funcoes.js
@@ -1,13 +1,28 @@
-function dirigirEBeber(Idade) {
- if(Idade >= 18 ){
-     console.log("Já pode dirigir e beber!");
- } else{
-     console.log("Não pode dirigir");
- }
-         
+/*function dirigirEBeber(Idade) {
+    if (Idade >= 18) {
+        console.log("Já pode dirigir e beber!");
+    } else {
+        console.log("Não pode dirigir");
+    }
+
 }
 
-function previsaoRodagem (litros,km) {
-    return (litros/km)
-    
+function previsaoRodagem(litros, km) {
+    return (litros / km)
+
+}*/
+
+function validaEmail(email) {
+if (email.indexOf("@") > -1) {
+    return document.write("email correto!");
+}else{
+    return document.write("email incorreto");
 }
+
+}
+
+var email = prompt("qual seu email")
+validaEmail(email) 
+
+
+    
